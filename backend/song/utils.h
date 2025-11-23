36bebb12bc95679c208bc65c3a9e46295ac86edf
SergioF21 <sergio.fierro@utec.edu.pe>
2025-11-22 19:48:22 -0500

    details

--- backend/song/utils.h
+++ backend/song/utils.h
@@ -4,9 +4,12 @@
 #include <vector>
 #include <cuda.h>
 #include <cuda_runtime.h>
+
 void checkCudaError(cudaError_t err, const char* msg);
 int atomicAddWrapper(int* address, int val);
 int divUp(int a, int b);
+int getNextPowerOfTwo(int n);
+std::vector<int> generateRandomIndices(int total, int sample_size, int seed =12345);
 
 
 #endif // UTILS_H
